8fb044029bd9b51097932b8a9b78bdfcc6c1e76a
selvin Chandrakanth <selvinchandrakanthc@gmail.com>
2016-08-15 01:24:52 +0700

    my first file to enter github hi

---
+++ arithmatic.py
@@ -0,0 +1,25 @@
+''''
+"arithmatic operations we can give the input directly for a and b
+and get the output from c"
+'''
+
+
+print("Welcome to Python..")
+print("Arithmatic operators..")
+a=input("enter the value of a:")
+b=input("enter the value of b:")
+c=int(a)+int(b)
+print("Sum:",c)
+c=int(a)-int(b)
+print("difference:",c)
+c=int(a)*int(b)
+print("Multiplication:",c)
+c=int(a)/int(b)
+print("Divisiom:",c)
+c=int(a)**int(b)
+print("power:",c)
+c=int(a)//int(b)
+print("floor division:",c)
+c=int(a)%int(b)
+print("Remainder:",c)
+        
